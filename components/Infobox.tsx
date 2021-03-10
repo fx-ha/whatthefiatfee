da0982b8ee98a2df99bff4b55371d7e72be0869a
fhaenel <fh@agentur-brandmarker.de>
2021-03-10 12:43:50 +0100

    fix eu currency bug

--- components/Infobox.tsx
+++ components/Infobox.tsx
@@ -17,13 +17,13 @@ const Infobox = ({
   })
   switch (currency) {
     case 'eur':
-      fiatValue = Number(currentData[0].usd).toLocaleString('de-DE', {
+      fiatValue = Number(currentData[0].eur).toLocaleString('de-DE', {
         style: 'currency',
         currency: currency,
       })
       break
     case 'gbp':
-      fiatValue = Number(currentData[0].usd).toLocaleString('en-GB', {
+      fiatValue = Number(currentData[0].gbp).toLocaleString('en-GB', {
         style: 'currency',
         currency: currency,
       })
